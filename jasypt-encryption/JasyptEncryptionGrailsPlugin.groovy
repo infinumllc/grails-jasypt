bacd197d51986daac5dab2dc48ffe23784e1792a
Ted Naleid <contact@naleid.com>
2010-06-27 00:22:20 -0500

    adding jasypt-encryption directory with clean plugin

---
+++ jasypt-encryption/JasyptEncryptionGrailsPlugin.groovy
@@ -0,0 +1,49 @@
+class JasyptEncryptionGrailsPlugin {
+    // the plugin version
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.3.2 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [:]
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    def author = "Ted Naleid"
+    def authorEmail = "contact@naleid.com"
+    def title = "Plugin summary/headline"
+    def description = '''\\
+Grails integration with Jasypt, allows easy encryption of information, including Hibernate/GORM integration.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/jasypt-encryption"
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before 
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+}
