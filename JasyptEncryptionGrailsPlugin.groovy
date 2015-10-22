e262f2c2ba5da01d5773ebef9f17b0077a94f2be
Daniel Bower <daniel.d.bower@gmail.com>
2015-10-22 15:41:48 -0400

    Add explicit group id

--- JasyptEncryptionGrailsPlugin.groovy
+++ JasyptEncryptionGrailsPlugin.groovy
@@ -11,6 +11,7 @@ class JasyptEncryptionGrailsPlugin {
         Security.addProvider(new BouncyCastleProvider())
     }
 
+    def group = "org.grails.plugins"
     def version = "1.1.0"
 
     def grailsVersion = "2.2.0 > *"
